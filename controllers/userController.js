7cdd019cab9a7259bb828b6058cc2f1addbc4ffc
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-09 13:35:47 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,18 +53,18 @@ class UserController {
         user.update({ confirmationCode: confirmationCode }, { where: { email: email } })
 
         let transporter = nodemailer.createTransport({
-            host: 'smtp.mail.ru',
+            host: 'mail.hosting.reg.ru',
             port: 465,
             secure: true,
             auth: {
-                user: 'radar.analytica@mail.ru',
-                pass: 'mgKvHuuHK8xXZnt33SGM',
+                user: 'radar-analytica@radar-analytica.ru',
+                pass: 'glebarioO1!',
             },
         });
 
 
         let result = await transporter.sendMail({
-            from: 'radar.analytica@mail.ru',
+            from: 'radar-analytica@radar-analytica.ru',
             to: email,
             subject: 'Подтверждение регистрации',
             text: 'Данное письмо отправлено с сервиса Radat Analytica',
