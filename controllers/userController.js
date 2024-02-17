0aefabcec505200e37139f8753d8372ed31353c1
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-17 22:07:59 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -316,8 +316,8 @@ class UserController {
 
             const urls = [
                 `https://suppliers-api.wildberries.ru/api/v3/warehouses`,
-                // `https://suppliers-api.wildberries.ru/api/v3/supplies?dateFrom=${dateFrom}`,
-                // `https://suppliers-api.wildberries.ru/api/v3/supplies?dateFrom=${dateFrom}`,
+                `https://suppliers-api.wildberries.ru/api/v3/supplies?dateFrom=${dateFrom}`,
+                `https://suppliers-api.wildberries.ru/api/v3/supplies?dateFrom=${dateFrom}`,
                 `https://suppliers-api.wildberries.ru/api/v3/supplies/orders/reshipment`,
                 `https://statistics-api.wildberries.ru/api/v1/supplier/incomes?dateFrom=${dateFrom}`,
                 `https://statistics-api.wildberries.ru/api/v1/supplier/stocks?dateFrom=${dateFrom}`,
@@ -333,8 +333,8 @@ class UserController {
 
             const names = [
                 'warehouses',
-                // 'supplies',
-                // 'newOrders',
+                'supplies',
+                'newOrders',
                 'reshipmentOrders',
                 'incomes',
                 'stocks',
