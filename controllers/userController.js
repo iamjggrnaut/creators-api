89669ea5243f08d1af68a020999a91e2de10df9b
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-19 10:55:24 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -198,7 +198,7 @@ class UserController {
         }
         let comparePassword = bcrypt.compareSync(password, user.password)
         if (!comparePassword) {
-            return next(ApiError.internal('Неверный логин или пароль'))
+            res.status(500).json({ success: false, message: 'Неверный логин или пароль' })
         }
         if (!comparePassword && !user.confirmed) {
             return next(ApiError.internal('Wrong password or email is not confirmed'))
