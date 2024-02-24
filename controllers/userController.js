3dc90c94026f0e6c477764509d758c11e1300a8d
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-24 17:09:29 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const fetchAndStore = require('../service/scheduler')
+const { promisify } = require('util');
 
 
 async function fetchData(url, resToken) {
