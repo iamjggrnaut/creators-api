aaea9fa0b3f169ad221a843d3e4cd7027b30eb2a
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-25 10:47:04 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userRouter = require('./userRouter')
+const dataCollectionRouter = require('./dataCollectionRouter')
 
 router.use('/user', userRouter)
-router.use('/data-collection', userRouter)
+router.use('/data-collection', dataCollectionRouter)
 
 
 module.exports = router
