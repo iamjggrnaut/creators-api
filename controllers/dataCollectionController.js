358c09ce8c1ec20f98c2313e21ec642fd7acbd22
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-25 20:03:20 +0300

    update

--- controllers/dataCollectionController.js
+++ controllers/dataCollectionController.js
@@ -26,7 +26,7 @@ module.exports = new DataCollectionController()
 function filterArrays(obj, days) {
     for (let key in obj) {
         if (Array.isArray(obj[key])) {
-            if (typeof obj[key] === 'object' && obj[key].length) {
+            if (obj[key].length) {
                 obj[key] = obj[key].filter(item => {
                     const date = item.date ? new Date(item.date) : item.lastChangeDate ? new Date(item.lastChangeDate) : new Date(item.create_dt);
                     const weekAgo = new Date(Date.now() - days * 24 * 60 * 60 * 1000);
