a10aa0942388ebb45329a8fa7e5d774e3d805232
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-25 18:09:03 +0300

    update

--- controllers/dataCollectionController.js
+++ controllers/dataCollectionController.js
@@ -5,6 +5,9 @@ class DataCollectionController {
     async getDataCollection(req, res) {
         const { id } = req.params
         const data = await DataCollection.findOne({ where: { userId: id } })
+        console.log(await DataCollection.findAll());
+        console.log('_____________________');
+        console.log(data);
         return res.json(data)
     }
 
