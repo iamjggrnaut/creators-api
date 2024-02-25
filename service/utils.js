8c75ff20229077a481059452b4b3f442c79bc728
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-26 00:12:44 +0300

    update

--- service/utils.js
+++ service/utils.js
@@ -1,7 +1,8 @@
 function filterArrays(obj, days) {
     for (let key in obj) {
         if (Array.isArray(obj[key])) {
-            if (obj[key].length && key !== 'warehouses') {
+            console.log(key);
+            if (obj[key].length) {
                 obj[key] = obj[key].filter(item => {
                     const date = item.date ? new Date(item.date) : item.lastChangeDate ? new Date(item.lastChangeDate) : new Date(item.create_dt);
                     const weekAgo = new Date(Date.now() - days * 24 * 60 * 60 * 1000);
