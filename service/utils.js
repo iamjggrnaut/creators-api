8c1fe8a10e29af83d6714d11997ffb42984b56db
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-26 16:19:38 +0300

    update

--- service/utils.js
+++ service/utils.js
@@ -151,6 +151,11 @@ function calculateBuyout(data, days) {
 }
 
 
+function dateMatches(date1, date2) {
+    return date1.toISOString().split('T')[0] === date2.toISOString().split('T')[0];
+}
+
+
 module.exports = {
     filterArrays,
     calculateOrders,
