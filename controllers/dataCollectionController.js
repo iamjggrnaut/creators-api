40f6286ea080e454956ceda21d3fa19d5f9177fb
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-26 12:13:28 +0300

    update

--- controllers/dataCollectionController.js
+++ controllers/dataCollectionController.js
@@ -1,5 +1,5 @@
 const { User, DataCollection } = require('../models/models')
-const { filterArrays } = require('../service/utils')
+const { filterArrays, calculateOrders } = require('../service/utils')
 
 class DataCollectionController {
 
@@ -12,10 +12,48 @@ class DataCollectionController {
     async getFilteredCollection(req, res) {
         const { id } = req.params
         const { days } = req.query
+
+        let content = {
+            orders: null,
+            sales: null,
+            returned: null,
+            buyout: null,
+            averageCheck: null,
+            chartData: null,
+            initialPrice: null,
+            penalty: null,
+            additionalPayment: null,
+            wbComission: null,
+            logistics: null,
+            marginRevenue: null,
+            lostProfit: null,
+            profit: null,
+            marginCosts: null,
+            grossProfit: null,
+            tax: null,
+            netProfit: null,
+            averageProfit: null,
+            buyoutPercentage: null,
+            roi: null,
+            vpProfitMargin: null,
+            opProfitMargin: null,
+            yearProfitMargin: null,
+            fbo: null,
+            fbs: null,
+            toClient: null,
+            fromClient: null,
+            notSorted: null,
+            advertisment: null,
+            commissionFromProfit: null,
+            logisticsFromProfit: null,
+            abcAnalysis: null,
+        }
+
         const data = await DataCollection.findOne({ where: { userId: id } })
+        const stats = calculateOrders(data, days)
         // const filtered = filterArrays(data.dataValues, days)
         // console.log(filtered);
-        return res.json(data)
+        return res.json({ data, stats })
     }
 
 }
--- service/utils.js
+++ service/utils.js
@@ -1,3 +1,13 @@
+function getDatesInInterval(startDate, endDate) {
+    const dates = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+        dates.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dates;
+}
+
 function filterArrays(obj, days) {
     for (let key in obj) {
         if (Array.isArray(obj[key])) {
@@ -14,5 +24,58 @@ function filterArrays(obj, days) {
     return obj
 }
 
+function calculateOrders(data, days) {
 
-module.exports = { filterArrays }
+    const currentDate = new Date();
+    // Получение даты 14 дней назад
+    const lastDaysDate = new Date(currentDate);
+    lastDaysDate.setDate(lastDaysDate.getDate() - days);
+    // Получение даты 28 дней назад (предыдущие days дней)
+    const previousDaysDate = new Date(lastDaysDate);
+    previousDaysDate.setDate(previousDaysDate.getDate() - days);
+    // Получение всех дней в последних 14 днях и предыдущих 14 днях
+    const lastDays = getDatesInInterval(lastDaysDate, currentDate);
+    const previousDays = getDatesInInterval(previousDaysDate, lastDaysDate);
+
+    function dateMatches(date1, date2) {
+        return date1.toISOString().split('T')[0] === date2.toISOString().split('T')[0];
+    }
+
+    const dataInLastDays = data.filter(item => {
+        const itemDate = new Date(item.date);
+        return lastDays.some(date => dateMatches(itemDate, date));
+    });
+
+    const dataInPreviousDays = data.filter(item => {
+        const itemDate = new Date(item.date);
+        return previousDays.some(date => dateMatches(itemDate, date));
+    });
+
+    function getTotalCostInPeriod(data) {
+        return data.reduce((total, item) => total + item.finishedPrice, 0);
+    }
+
+    const totalCostInLastDays = getTotalCostInPeriod(dataInLastDays);
+    const totalCostInPreviousDays = getTotalCostInPeriod(dataInPreviousDays);
+    const percentPriceChange = ((totalCostInLastDays - totalCostInPreviousDays) / totalCostInPreviousDays) * 100;
+    const percentAmountChange = ((dataInLastDays.length - dataInPreviousDays.length) / dataInPreviousDays.length) * 100;
+
+    let amount = dataInLastDays.length
+    let priceArrayCurrent = dataInLastDays.map(item => item.finishedPrice)
+    let sum = priceArrayCurrent.reduce((a, b) => a + b, 0)
+
+    const revenueIncrease = (totalCostInLastDays - totalCostInPreviousDays) / lastDays.length;
+    const amountIncrease = (dataInLastDays.length - dataInPreviousDays.length) / lastDays.length;
+
+    return {
+        amount: amount,
+        sum: sum,
+        sumPercent: percentPriceChange.toFixed(2),
+        amountPercent: percentAmountChange.toFixed(2),
+        revenueIncrese: revenueIncrease,
+        amountIncrese: amountIncrease,
+    }
+}
+
+
+module.exports = { filterArrays, calculateOrders }
