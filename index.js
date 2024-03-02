0229a5f41969d406c803c403da51416394cc4662
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-03-03 01:39:36 +0300

    update

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const models = require('./models/models')
-const multer = require('multer')
-const upload = multer({ dest: './static/' })
-const PORT = process.env.PORT || 5000
-const cors = require('cors')
-const router = require('./routes/index')
-const fs = require('fs')
-require('./service/scheduler');
+const cluster = require('cluster');
+const os = require('os');
 
-const app = express()
+if (cluster.isMaster) {
+    const numCPUs = os.cpus().length;
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    require('dotenv').config();
+    const express = require('express');
+    const sequelize = require('./db');
+    const multer = require('multer');
+    const PORT = process.env.PORT || 5000;
+    const cors = require('cors');
+    const router = require('./routes/index');
+    require('./service/scheduler');
 
-app.use('/static', express.static('static'))
-app.use(cors())
+    const app = express();
 
-// app.post('/api/uploadFile', upload.single('static'), (req, res) => {
-//     const fileType = req.file.mimetype.split('/')[1]
-//     const newName = req.file.filename + '.' + fileType
-//     fs.rename('./static/' + req.file.filename, './static/' + newName, () => {
-//         res.send(newName)
-//     })
-// })
+    app.use('/static', express.static('static'));
+    app.use(cors());
+    app.use(express.json());
+    app.use('/api', router);
 
+    const start = async () => {
+        try {
+            await sequelize.authenticate();
+            await sequelize.sync();
+            app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-
-app.use(express.json())
-app.use('/api', router)
-
-const start = async () => {
-    try {
-
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
-    } catch (e) { console.log(e); }
+    start();
 }
-
-start()
