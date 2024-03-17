33c13fccfe4babc829e6cbf924b1ab2fb6b72f87
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-03-17 21:17:43 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,21 +83,21 @@ class UserController {
                 ],
                 html:
                     `<div style="padding: 1rem; background-color: white; width: 100%; margin: 0 auto; ">
-                    <div style="padding: 1rem; max-width: 560px; width: 100%; margin: 0 auto; ">
+                    <div style="padding: 1rem; max-width: 560px; margin: 0 auto; ">
                         <img src="cid:unique-image-id" alt="Изображение" style="width: 200px;">
                         <h1>Здраствуйте, ${lastName}!</h1>
                         <p style="color: #8C8C8C;">Осталось совсем чуть-чуть</p>
                         <p>Подтвердите регистрацию:</p>
                         <br />
-                        <div style="display: flex; width: width: 100%; text-align: center; margin: 20px auto;">
-                        <a href="https://radar-analytica.ru/development/confirmation/${email}/${confirmationCode}" style='border: none; margin: 8px auto; background-color: #5329FF; color: white; border-radius: 8px; padding: 20px auto; font-weight: 700;text-decoration: none; width: 100%;'>Подтвердить</a>
+                        <div style="display: flex; width: 100%; max-width: 700px; text-align: center; margin: 20px auto;">
+                        <a href="https://radar-analytica.ru/development/confirmation/${lastName}/${confirmationCode}" style='border: none; margin: 8px auto; background-color: #5329FF; color: white; border-radius: 8px; padding: 20px 32px; font-weight: 700;text-decoration: none; width: 100%;'>Подтвердить</a>
                         </div>
                         <br />
                         <p>C наилучшими пожеланиями,</p>
                         <p>Команда сервиса Radar Analytica</p>
                         <br />
                     </div>
-                    <div style="background-color: rgba(247, 246, 254, 1); padding: 1rem; border-radius: 4px; max-width: 560px; width: 100%; margin: 0 auto;">
+                    <div style="background-color: rgba(247, 246, 254, 1); padding: 1rem; border-radius: 4px; max-width: 560px; margin: 0 auto;">
                         <p>Вы получили это письмо, так как зарегистрировались на сайте</p>
                         <a href="https://radar-analytica.ru">https://radar-analytica.ru</a>
                         <br>
@@ -149,7 +149,7 @@ class UserController {
             ],
             html:
                 `<div style="padding: 1rem; background-color: white; margin: 0 auto">
-                    <div style="padding: 1rem; max-width: 560px; width: 100%; margin:0 auto">
+                    <div style="padding: 1rem; max-width: 560px; margin:0 auto">
                         <img src="cid:unique-image-id" alt="Изображение" style="width: 200px;">
                         <h1>Восстановление пароля</h1>
                         <p style="color: #8C8C8C;">Не переживайте, это несложно и безопасно</p>
@@ -160,7 +160,7 @@ class UserController {
                         <p>Команда сервиса Radar Analytica</p>
                         <br />
                     </div>
-                    <div style="background-color: rgba(247, 246, 254, 1); padding: 1rem; border-radius: 4px; max-width: 560px; width: 100%; margin:0 auto">
+                    <div style="background-color: rgba(247, 246, 254, 1); padding: 1rem; border-radius: 4px; max-width: 560px; margin:0 auto">
                         <p>Вы получили это письмо, так как запросили восстановление пароля на сайте</p>
                         <a href="https://radar-analytica.ru">https://radar-analytica.ru</a>
                         <br>
