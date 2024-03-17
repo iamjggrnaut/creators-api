b6b77ce1cdf11783771455300734f698d008b976
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-03-17 20:26:01 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -223,11 +223,11 @@ class UserController {
                     `<div style="padding: 1rem; background-color: white; margin: 0 auto;">
                             <div style="padding: 1rem; max-width: 560px; margin: 0 auto; ">
                                 <img src="cid:unique-image-id" alt="Изображение" style="width: 200px;">
-                                <h1>Здраствуйте, ${lastName}!</h1>
+                                <h1>Здраствуйте, ${email}!</h1>
                                 <p style="color: #8C8C8C;">Осталось совсем чуть-чуть</p>
                                 <p>Подтвердите регистрацию:</p>
                                 <div style="display: flex; max-width: 560px; text-align: center; margin: 20px auto;">
-                                <a href="https://radar-analytica.ru/development/confirmation/${email}/${confirmationCode}" style='border: none; margin: 8px auto; background-color: #5329FF; color: white; border-radius: 8px; padding: 20px 32px; font-weight: 700;text-decoration: none; width: 100%;'>Подтвердить</a>
+                                <a href="https://radar-analytica.ru/development/confirmation/${email}/${''}" style='border: none; margin: 8px auto; background-color: #5329FF; color: white; border-radius: 8px; padding: 20px 32px; font-weight: 700;text-decoration: none; width: 100%;'>Подтвердить</a>
                                 </div>
                                 <p>C наилучшими пожеланиями,</p>
                                 <p>Команда сервиса Radar Analytica</p>
